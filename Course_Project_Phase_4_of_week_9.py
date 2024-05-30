a6da67af070288389a4de33f3f89c021f9b91e67
username <useremail>
2024-05-29 18:49:51 -0700

    Add project files.

---
+++ Course_Project_Phase_4_of_week_9.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+def CreateUsers():
+    print('##### Create users, passwords, and roles #####')
+    UserFile = open("Users.txt", "a+")
+    while True:
+        username = GetUserName()
+        if (username.upper() == "END"):
+            break
+        userpwd = GetUserPassword()
+        userrole = GetUserrole()
+        
+        UserDetail = username + "|" + userpwd + "|" + userrole + "\n"
+        UserFile.write(UserDetail)
+        
+    UserFile.close()
+    printuserinfo()
+    
+def GetUserName():
+    username = input("Enter user name or 'End' to quit: ")
+    return username
+
+def GetUserPassword():
+    pwd = input("Enter password: ")
+    return pwd
+
+def GetUserRole():
+    userrole = input("Enter role (Admin or User): ")
+    while True:
+        if (userrole.upper() == "ADMIN" or userrole.upper() == "USER"):
+            return userrole
+        else:
+            userrole = input("Enter a user role (Adam or User): ")
+            
